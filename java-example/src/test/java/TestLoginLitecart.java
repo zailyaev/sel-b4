be95d48d4a5f73abc68f20622fde885c669457c4
zailyaev <zailyaev@action-media.ru>
2019-04-17 18:59:39 +0300

    добавил проверку в конце кейса

--- java-example/src/test/java/TestLoginLitecart.java
+++ java-example/src/test/java/TestLoginLitecart.java
@@ -3,10 +3,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class TestLoginLitecart {
 
@@ -18,6 +14,7 @@ public class TestLoginLitecart {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+        driver.findElement(By.cssSelector("#box-widgets")).isDisplayed();
     }
 
     @After
