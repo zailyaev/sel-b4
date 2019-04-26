0980d0e87462a6c13245e3d292802aa6c0cc89bb
zailyaev <zailyaev@action-media.ru>
2019-04-26 14:52:52 +0300

    сделан кейс по проверке стикеров

--- java-example/src/test/java/TestStickers.java
+++ java-example/src/test/java/TestStickers.java
@@ -1,24 +1,34 @@
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestStickers {
+    private WebDriver driver;
 
-    private WebDriver driver = new ChromeDriver();
-
-    @Test
-    public void test() {
-
-        driver.manage().window().maximize();
-        driver.navigate().to("http://localhost/litecart/en/");
-        driver.findElement(By.xpath("(//div[@class='name'][contains(.,'Duck')])"));
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
-
+    @Test
+    public void testStickerCheck() {
+        driver.manage().window().maximize();
+        driver.get("http://localhost/litecart/en/");
+        int ducks = driver.findElements(By.cssSelector(".product")).size();
+        for (int i = 0; i < ducks; i++){
+            Assert.assertEquals(driver.findElements(By.cssSelector(".product")).get(i)
+                    .findElements(By.cssSelector(".sticker")).size(),1);
+        }
+    }
     @After
-    public void stopDriver() {
+    public void stop () {
         driver.quit();
+        driver = null;
     }
 }
