6802a73f56272e6ef63424f8dc54748ffe1dc4c4
zailyaev <zailyaev@action-media.ru>
2019-04-30 22:29:17 +0300

    правки после ревью "TestGeozonesSort"

--- java-example/src/test/java/TestGeozonesSort.java
+++ java-example/src/test/java/TestGeozonesSort.java
@@ -1,13 +1,14 @@
-import com.google.common.collect.Ordering;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -30,22 +31,25 @@ public class TestGeozonesSort {
         driver.findElement(By.name("login")).click();
 
         int zone = driver.findElements(By.cssSelector(".dataTable .row")).size();
-        for (int a = 0; a < zone; a++){
+        for (int a = 0; a < zone; a++) {
             driver.findElements(By.cssSelector(".dataTable .row"))
                     .get(a).findElement(By.cssSelector(".fa-pencil")).click();
+            int zoneCode = driver.findElements(By.xpath("(//select[contains(@name,'zone_code]')])")).size();
             List<String> zones = new ArrayList<String>();
-            getZonesList(zones);
-            boolean sorted = Ordering.natural().isOrdered(zones);
-            Assert.assertTrue(sorted);
+            for (int b = 0; b < zoneCode; b++) {
+                List<WebElement> options = driver.findElements(By.xpath("(//select[contains(@name,'zone_code]')])"))
+                        .get(b).findElements(By.cssSelector("option"));
+                for (WebElement option : options) {
+                    if ("true".equals(option.getAttribute("selected"))) {
+                        String optionText = option.getText();
+                        zones.add(optionText);
+                    }
+                }
+                List<String> sortedZoneList = zones;
+                sortedZoneList.sort(Comparator.naturalOrder());
+                Assert.assertEquals(zones, sortedZoneList);
+            }
             driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
-        }
-    }
-
-    private void getZonesList(List<String> zones) {
-        int zoneCode = driver.findElements(By.xpath("(//select[contains(@name,'zone_code]')])")).size();
-        for (int b = 0; b < zoneCode; b++){
-            int count = driver.findElements(By.xpath("(//select[contains(@name,'zone_code]')])"))
-                    .get(b).findElements(By.cssSelector("option")).size();
         }
     }
 
